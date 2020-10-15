70cbb95afa4d3036ff44be4dacf97c93a2676b10
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-15 15:56:50 +0530

    add authenticate, home wrap files

---
+++ lib/screen/Wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:logging_app/screen/home/Home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
